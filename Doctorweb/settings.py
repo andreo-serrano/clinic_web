13a4f3cc143a4171bbef9b383fcbcb6bea46b093
andreo-serrano <90542803+andreo-serrano@users.noreply.github.com>
2024-12-17 06:18:53 +0800

    Update settings.py

--- Doctorweb/settings.py
+++ Doctorweb/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Doctorweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / "myapp/Templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
